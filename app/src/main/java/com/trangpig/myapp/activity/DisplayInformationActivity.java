be8f5d96a890fef2b67d6ed839b26b64730272fa
12130204 <12130204@st.hcmuaf.edu.vn>
2015-06-27 22:48:55 +0700

    edit nganh

--- app/src/main/java/com/trangpig/myapp/activity/DisplayInformationActivity.java
+++ app/src/main/java/com/trangpig/myapp/activity/DisplayInformationActivity.java
@@ -1,18 +1,80 @@
 package com.trangpig.myapp.activity;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import com.nhuocquy.model.Student;
 import com.trangpig.myapp.R;
+import com.trangpig.myapp.fragment.SearchScoreFragment;
+import com.trangpig.until.MyUri;
+
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+import java.text.SimpleDateFormat;
 
 public class DisplayInformationActivity extends Activity {
-
+    TextView tvSBD,tvName,tvSex, tvAddress,tvBirdth,tvDeparment, tvGradutedSchool, tvGradutedSchoolCode;
+    ListView lvScore;
+    RestTemplate restTemplate;
+    String sbd;
+    SimpleDateFormat f;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_information);
+
+        tvSBD = (TextView) findViewById(R.id.tvHTSBD);
+        tvName = (TextView) findViewById(R.id.tvTen);
+        tvSex = (TextView) findViewById(R.id.tvHTSex);
+        tvAddress = (TextView) findViewById(R.id.tvHTNS);
+        tvBirdth = (TextView) findViewById(R.id.tvNgayHienThi);
+        tvDeparment = (TextView) findViewById(R.id.tvHTNganh);
+        tvGradutedSchool = (TextView) findViewById(R.id.tvHTTrg);
+        tvGradutedSchoolCode = (TextView) findViewById(R.id.tvHTMaTrg);
+
+
+        sbd = getIntent().getStringExtra(SearchScoreFragment.SBD_STUDENT);
+        f = new SimpleDateFormat("dd/MM/yyyy");
+        restTemplate = new RestTemplate();
+        restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+
+        new AsyncTask<Void, Void, Void>() {
+            Student student;
+
+            @Override
+            protected Void doInBackground(Void... params) {
+                try {
+                    student = restTemplate.getForObject(String.format(MyUri.URL_FIND_STUDENT_BY_SBD, MyUri.IP, sbd), Student.class);
+                } catch (RestClientException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                tvSBD.setText(student.getSBD());
+                tvName.setText(student.getName());
+                if(student.isSex()){
+                    tvSex.setText("Nu");
+                }else {
+                    tvSex.setText("Nam");
+                }
+                tvAddress.setText(student.getAddress());
+                tvBirdth.setText(f.format(student.getBirthday()));
+                tvDeparment.setText(student.getSchoolRegisterCode());
+                tvGradutedSchool.setText(student.getSchoolName());
+                tvGradutedSchoolCode.setText(student.getSchoolCode());
+            }
+        }.execute();
+
     }
 
     @Override
